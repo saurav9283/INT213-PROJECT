d868cbd50db2f60db9c1ae33590c9ef9eb9ba5db
saurav Kumar <87579538+saurav9283@users.noreply.github.com>
2021-11-30 11:33:10 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,14 +1,16 @@
 import time
-import winsound
 from tkinter import *
 import datetime
 from tkinter.ttk import *
+#global variables for stopwatch and timer
 stopwatch_counter_num=66600
 stopwatch_running=False
 timer_counter_num=66600
 timer_running=False
+#Clock Function which controls the Time and Date Labels.
 def clock():
-    date_time=datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S/%p")
+    date_time=datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S/%p") #taking Date and Time
+    #converting in PM and AM time
     date,time1=date_time.split()
     time2,time3=time1.split('/')
     hour,minute,second=time2.split(':')
@@ -20,29 +22,35 @@ def clock():
     current_time.config(text=time)
     date_label.config(text=date)
     time_label.after(1000,clock)
-
+#Creating alarm Function which is called by The alarm_button
 def alarm():
-    main_time=datetime.datetime.now().strftime("%H:%M %p")
+    main_time=datetime.datetime.now().strftime("%H:%M %p") #Taking  time form datetime module and formating it
+    #spliting maintime and alarm time in hour,minute,second,AM/PM
     main_time1,main_time2=main_time.split(' ')
     main_hour1,main_minute=main_time1.split(":")
     alarm_time=alarm_input.get()
     alarm_time1,alarm_time2=alarm_time.split(' ')
     alarm_hour,alarm_minute=alarm_time1.split(":")
+    #converting maintime in PM and AM time
     if int(main_hour1)>12 and int(main_hour1)<=24:
         main_hour=str(int(main_hour1)-12)
     else:
         main_hour=main_hour1
+    # checking if the time entered is same or not
     if int(main_hour)==int(alarm_hour) and int(main_minute)==int(alarm_minute) and main_time2==alarm_time2:
+        #changing alarm status label
         alarm_status.config(text="Weak Up")
         alarm_button.config(state="enabled")
         alarm_input.config(state="enabled")
 
     else:
+        # changing alarm status label
         alarm_status.config(text="Alarm has started")
         alarm_button.config(state="disabled")
         alarm_input.config(state="disabled")
     alarm_status.after(1000,alarm)
 
+# This function adds 1 to the Stopwatch counter num in every one second
 def stopwatch_counter(label):
     def counter():
         if stopwatch_running:
@@ -54,7 +62,7 @@ def stopwatch_counter(label):
             label.after(1000,counter)
             stopwatch_counter_num+=1
     counter()
-
+#stopwatch function which controls the stopwatch and is called by the Stopwatch Buttons
 def stopwatch(work):
     global stopwatch_running
     if work=="start":
@@ -76,6 +84,7 @@ def stopwatch(work):
         stopwatch_stop.config(state="disabled")
         stopwatch_rest.config(state="disabled")
         stopwatch_label.config(text="00:00:00")
+#this function subtracts 1 to the Timer counter num in every one second.
 def timer_counter(label):
 
     def count():
@@ -94,7 +103,7 @@ def timer_counter(label):
             label.config(text=display)
             label.after(1000, count)
     count()
-
+#Timer function which controls the Timer and is called by the Timer Buttons
 def timer(work):
     if work=="start":
         global timer_running, timer_counter_num
@@ -126,10 +135,11 @@ def timer(work):
         timer_entry.delete(0, END)
 
 my_win=Tk()
-my_win.title("Clock")
-my_win.geometry("600x400")
+my_win.title("Clock") #title
+my_win.geometry("600x400") #setting  window size
 my_win.minsize(550,400)
 my_win.maxsize(600,400)
+#creating Tabs by using Notebook
 main_tab=Notebook(my_win)
 clock_tab=Frame(main_tab)
 alarm_tab=Frame(main_tab)
@@ -142,22 +152,30 @@ main_tab.add(timer_tab,text="Timer")
 main_tab.pack(expand=1,fill="both")
 clock_label=Label(clock_tab,text="Clock",foreground="red",font="calibri 45 bold")
 clock_label.pack(pady=20)
+              #clock
+#creating time and date labels which will get data from clock function
 time_label=Label(clock_tab,foreground="green",font="calibri 35 bold")
 time_label.pack(pady=5)
 date_label=Label(clock_tab,foreground="green",font="calibri 35 bold")
 date_label.pack()
+#creating Alarm and current_time labels,current time label get input from clock function
 alarm_label=Label(alarm_tab,text="Alarm",foreground="red",font="calibri 45 bold")
 alarm_label.pack(pady=20)
 current_time=Label(alarm_tab,font="calibri 12 bold")
 current_time.pack(pady=5)
+                   #Alarm
+# Creating Alarm Entry Box, Alarm Instructions and Set Alarm button which call alarm function
 alarm_input=Entry(alarm_tab,font="calibri 15 bold")
 alarm_input.pack()
 alarm_input_details=Label(alarm_tab,font = 'calibri 10 bold',text = "Enter Alarm Time. Eg -> 01:30 PM, 01 -> Hour, 30 -> Minutes")
 alarm_input_details.pack()
 alarm_button=Button(alarm_tab,text="Set Alarm",command=alarm)
 alarm_button.pack(pady=20)
+#Creating Alarm Status Label
 alarm_status=Label(alarm_tab,font = 'calibri 15 bold')
 alarm_status.pack()
+            #stopwatch
+#Stopwatch Heading label ,stopwatch Label, Start , Pause, Reset Buttons which calls the stopwatch function.
 stopwatch_heading=Label(stopwatch_tab,text="Stopwatch",foreground="red",font="calibri 45 bold")
 stopwatch_heading.pack(pady=20)
 stopwatch_label = Label(stopwatch_tab, font='calibri 40 bold', text='00:00:00')
@@ -168,11 +186,13 @@ stopwatch_stop=Button(stopwatch_tab,text="Pause",state="disable",command=lambda 
 stopwatch_stop.pack(anchor="center",side=LEFT,padx=40)
 stopwatch_rest=Button(stopwatch_tab,text="Reset",state="disable",command=lambda :stopwatch("reset"))
 stopwatch_rest.pack(side=LEFT,padx=50,fill=X)
+                   #timer
+#Timer heading label,Timer entry, instructions label,timer label and Start, Stop, Reset Button which calls Timer function
 timer_label1=Label(timer_tab,text="Timer",foreground="red",font="calibri 45 bold")
 timer_label1.pack()
 timer_entry=Entry(timer_tab,font="calibri 15 bold")
 timer_entry.pack()
-timer_label2=Label(timer_tab,text="Enter Timer Time")
+timer_label2=Label(timer_tab,text="Enter Timer Time. Eg -> 01:30:30")
 timer_label2.pack()
 timer_label=Label(timer_tab,text="00:00:00",font="calibri 40 bold")
 timer_label.pack()
@@ -182,7 +202,8 @@ timer_stop=Button(timer_tab,text="Pause",state="disable",command=lambda :timer("
 timer_stop.pack(anchor="center",side=LEFT,padx=40)
 timer_rest=Button(timer_tab,text="Reset",state="disable",command=lambda :timer("reset"))
 timer_rest.pack(side=LEFT,padx=50,fill=X)
+#creating Exit button with quit command
 end_button=Button(my_win,text="Exit",command=quit)
 end_button.pack(side=RIGHT)
-clock()
+clock() #starting clock function
 my_win.mainloop()
